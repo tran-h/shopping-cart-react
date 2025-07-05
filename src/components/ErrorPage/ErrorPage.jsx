4a31ca89c3d5c4f8ef1b1dfc9a5d8bff1a245cb0
tran-h <43885150+tran-h@users.noreply.github.com>
2025-07-04 20:55:40 -0400

    feat: add content/styling to error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,16 @@
 import styles from "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>ErrorPage Page</h1>
-      <p>Welcome to the ErrorPage Page!</p>
+    <div className={styles.errorContainer}>
+      <div className={styles.errorContent}>
+        <h1>404</h1>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <Link to="/" className={styles.homeButton}>
+          Go Back Home
+        </Link>
+      </div>
     </div>
   );
 }
